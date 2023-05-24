6210f1bbfaaad79f48aad61b86f3b1fd8e4a425f
Godfrey Inyama <41163891+Godfrey2712@users.noreply.github.com>
2023-05-24 16:26:16 +0100

    tweak

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import redirect, request, render_template, jsonify
+from flask import request, render_template, Response
 from . import application
 import json
+import os
 from app.xaif import generate_predictions
 
 @application.route('/ya_predict', methods=['GET', 'POST'])
@@ -14,3 +15,25 @@ def amf_schemes():
         return (predictions)
     elif request.method == 'GET':
         return render_template('index.html')
+    
+# for the n8n system to convert from bjson back to json
+@application.route('/bson_json', methods=['GET', 'POST'])
+def remove_backslashes():
+    if request.method == 'POST':
+        fi = request.files['filei']
+        fi.save(fi.filename)
+        ffi = open(fi.filename, 'r')
+        content = ffi.read()
+        # Remove the backslashes
+        data = content.replace('\\', '')
+        #predictions = generate_predictions(data)
+        #return data
+        # Determine the file format of the input data
+        _, file_extension = os.path.splitext(fi.filename)
+        # Return the data with the file format
+        return Response(data, mimetype=f'application/{file_extension[1:]}')
+    elif request.method == 'GET':
+        return render_template('bjson.html')
+    
+if __name__ == '__main__':
+    application.run(debug=True)
