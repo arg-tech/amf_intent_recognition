a815dfc57e8fe274ba7bc9af0fc922ea8ffefab9
Godfrey <godfreyinyama@gmail.com>
2024-07-17 18:20:44 +0100

    port_change

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source venv/bin/activate
-exec gunicorn -b :5000 --access-logfile - --error-logfile - app --timeout 300
+exec gunicorn -b :5050 --access-logfile - --error-logfile - app --timeout 300
